f5045008fd514b557bf83d27e0802d5becf83139
Erik Poole <Mahkumazahn@gmail.com>
2018-08-20 11:34:29 -0600

    Committing Day One of C++ Hello World Project

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Erik Poole on 8/20/18.
+//  Copyright © 2018 ErikPoole. All rights reserved.
+
+/*
+    Lab 1 - "Hello World"
+*/
+
+//  Include Statements - iostream is a library used for most of the semester
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n\n";
+    std::cout << "Eat my shorts!\n";
+    std::cout << 2+2;
+    std::cout << "\n";
+    
+    return 258;
+}
