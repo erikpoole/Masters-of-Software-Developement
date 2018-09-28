ae5b9cf42f97da8904b0f4bc0a1c0331e3963212
Erik Poole <Mahkumazahn@gmail.com>
2018-09-28 12:16:16 -0600

    Changed to send all bytes at once instead of loop, fixed 404

--- 6011/hwBasicHTTPServer/src/hwBasicHTTPServer/Server.java
+++ 6011/hwBasicHTTPServer/src/hwBasicHTTPServer/Server.java
@@ -8,59 +8,53 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class Server {
-			
+
 	public void openServer(int socketNum) throws Exception {
 		ServerSocket myServer = new ServerSocket(socketNum);
 		while (true) {
 			System.out.println("Working");
 			Socket socket = myServer.accept();
-            
-            
-            //reads in argument sent by client
- 			Scanner input = new Scanner(socket.getInputStream());
- 			input.next();
- 			String filename = "Resources" + input.next();
- 			System.out.println(filename);
-			
 
-	 		byte [] fileBytes = {};	 		
- 			try {
- 	 			//reads in bytes from file
- 				if (filename.equals("Resources/")) {
- 					filename = "Resources/Index.html";
- 				}
- 	 			FileInputStream fileStream = new FileInputStream(filename);
- 	 			fileBytes = fileStream.readAllBytes();
- 	 			fileStream.close();
- 	 			
- 				//sends HTML protocol to client
- 				PrintWriter outputHeader = new PrintWriter(socket.getOutputStream());
- 				outputHeader.println("HTTP/1.1 1 OK");
- 				outputHeader.println("Content-Length: " + fileBytes.length);
- 	            outputHeader.println();
- 	            outputHeader.flush();
- 	 			
- 	 			//outputs bytes
- 	 			OutputStream outputBody = socket.getOutputStream();
- 	 			for (int i = 0; i < fileBytes.length; i++) {
- 	 				outputBody.write(fileBytes[i]);
- 	 			}
- 	 			outputBody.flush();
- 	 			
- 	 			//if filename not found throws 404 error
- 			} catch (Exception e) {
- 				PrintWriter outputHeader = new PrintWriter(socket.getOutputStream());
- 				outputHeader.println("HTTP/1.1 1 OK");
- 				outputHeader.println("Content-Length: 19");
- 	            outputHeader.println();
- 	            outputHeader.println("404: File Not Found");
- 	            outputHeader.flush();
- 			}
+			// reads in argument sent by client
+			Scanner input = new Scanner(socket.getInputStream());
+			input.next(); //fix me to need "GET", else exception
+			String filename = "Resources" + input.next();
+			System.out.println(filename);
 
- 			
+			byte[] fileBytes = {};
+			PrintWriter outputHeader = new PrintWriter(socket.getOutputStream());
+
+			try {
+				// reads in bytes from file
+				if (filename.equals("Resources/")) {
+					filename = "Resources/Index.html";
+				}
+				FileInputStream fileStream = new FileInputStream(filename);
+				// get cannonical path
+				fileBytes = fileStream.readAllBytes();
+				fileStream.close();
+
+				// sends HTML protocol to client
+				outputHeader.println("HTTP/1.1 200 OK");
+				outputHeader.println("Content-Length: " + fileBytes.length);
+				outputHeader.println();
+				outputHeader.flush();
+
+				// outputs bytes
+				OutputStream outputBody = socket.getOutputStream();
+				outputBody.write(fileBytes);
+				outputBody.flush();
+
+				// if filename not found throws 404 error
+			} catch (Exception e) {
+				outputHeader.println("HTTP/1.1 404 File Not Found");
+				outputHeader.println("Content-Length: 19");
+				outputHeader.println();
+				outputHeader.println("404: File Not Found");
+				outputHeader.flush();
+			}
+
 //////////////////////////////////////////////////////////////////////////////////////////
 //			DOES NOT WORK CONSISTENTLY! .available() = BAD!!!
 //			ArrayList<Byte> fileBytes = new ArrayList<Byte>();
@@ -68,8 +62,7 @@ public class Server {
 //			fileBytes.add((byte) fileStream.read());
 //			}
 //////////////////////////////////////////////////////////////////////////////////////////
- 		
- 			
+
 			input.close();
 			socket.close();
 		}
