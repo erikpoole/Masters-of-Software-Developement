ad0b43828bbdd7e2e6c556a80b30906ce87eb088
Erik Poole <Mahkumazahn@gmail.com>
2018-09-30 16:11:50 -0600

    Commit before big punge of breaking up server class

--- 6011/hwBasicHTTPServer/src/hwBasicHTTPServer/Main.java
+++ 6011/hwBasicHTTPServer/src/hwBasicHTTPServer/Main.java
@@ -1,12 +1,23 @@
 package hwBasicHTTPServer;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 
+		try {
 			Server server = new Server();
 			server.openServer(8080);
 
+		} catch (FileNotFoundException e) {
+//			outputHeader.println("HTTP/1.1 404 FILE NOT FOUND");
+//			outputHeader.println("Content-Length: 0");
+//			outputHeader.println();
+//			outputHeader.flush();
+//			input.close();
+
+		}
 
 	}
 
--- 6011/hwBasicHTTPServer/src/hwBasicHTTPServer/Server.java
+++ 6011/hwBasicHTTPServer/src/hwBasicHTTPServer/Server.java
@@ -1,67 +1,65 @@
 package hwBasicHTTPServer;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Server {
 
+	public static void respond404(int socketNum) {
+
+	}
+
 	public void openServer(int socketNum) throws Exception {
 		ServerSocket myServer = new ServerSocket(socketNum);
+
 		while (true) {
 			System.out.println("Working");
 			Socket socket = myServer.accept();
-
 			// reads in argument sent by client
 			Scanner input = new Scanner(socket.getInputStream());
-			input.next(); //fix me to need "GET", else exception
+			input.next(); // fix me to need "GET", else exception
 			String filename = "Resources" + input.next();
-			System.out.println(filename);
+			if (filename.equals("Resources/")) {
+				filename = "Resources/Index.html";
+			}
+			File file = new File(filename);
 
-			byte[] fileBytes = {};
 			PrintWriter outputHeader = new PrintWriter(socket.getOutputStream());
 
-			try {
-				// reads in bytes from file
-				if (filename.equals("Resources/")) {
-					filename = "Resources/Index.html";
-				}
-				FileInputStream fileStream = new FileInputStream(filename);
-				// get cannonical path
-				fileBytes = fileStream.readAllBytes();
-				fileStream.close();
-
-				// sends HTML protocol to client
-				outputHeader.println("HTTP/1.1 200 OK");
-				outputHeader.println("Content-Length: " + fileBytes.length);
-				outputHeader.println();
-				outputHeader.flush();
-
-				// outputs bytes
-				OutputStream outputBody = socket.getOutputStream();
-				outputBody.write(fileBytes);
-				outputBody.flush();
-
-				// if filename not found throws 404 error
-			} catch (Exception e) {
-				outputHeader.println("HTTP/1.1 404 File Not Found");
-				outputHeader.println("Content-Length: 19");
-				outputHeader.println();
-				outputHeader.println("404: File Not Found");
-				outputHeader.flush();
+			System.out.println(file.getCanonicalPath());
+			if (!file.getAbsolutePath().equals(file.getCanonicalPath())) {
+				throw new FileNotFoundException();
+//				outputHeader.println("HTTP/1.1 404 FILE NOT FOUND");
+//				outputHeader.println("Content-Length: 0");
+//				outputHeader.println();
+//				outputHeader.flush();
+//				input.close();
+//				continue;
 			}
 
-//////////////////////////////////////////////////////////////////////////////////////////
-//			DOES NOT WORK CONSISTENTLY! .available() = BAD!!!
-//			ArrayList<Byte> fileBytes = new ArrayList<Byte>();
-//			while (fileStream.available() > 0) {
-//			fileBytes.add((byte) fileStream.read());
-//			}
-//////////////////////////////////////////////////////////////////////////////////////////
+			byte[] fileBytes = {};
+			// reads in bytes from file
+			FileInputStream fileStream = new FileInputStream(file.getCanonicalPath());
+			// get cannonical path
+			fileBytes = fileStream.readAllBytes();
+			fileStream.close();
+
+			// sends HTML protocol to client
+			outputHeader.println("HTTP/1.1 200 OK");
+			outputHeader.println("Content-Length: " + fileBytes.length);
+			outputHeader.println();
+			outputHeader.flush();
+
+			// outputs bytes
+			OutputStream outputBody = socket.getOutputStream();
+			outputBody.write(fileBytes);
+			outputBody.flush();
 
 			input.close();
 			socket.close();
