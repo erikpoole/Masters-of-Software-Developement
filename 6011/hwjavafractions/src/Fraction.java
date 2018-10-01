35fb6ab8eb4f277ab921fda4114395b3dd4fe557
Erik Poole <Mahkumazahn@gmail.com>
2018-10-01 09:57:32 -0600

    Implemented Comparable into Fraction & tested Collections.sort

--- 6011/hwjavafractions/src/Fraction.java
+++ 6011/hwjavafractions/src/Fraction.java
@@ -1,112 +1,113 @@
-public class Fraction {
-			
-	//Greatest Common Denominator
-	private long calculateGCD() {
-	    long gcd = numerator;
-	    long remainder = denominator;
-	    while(remainder != 0) {
-	        long temp = remainder;
-	        remainder = gcd % remainder;
-	        gcd = temp;
-	    }
-	    return gcd;
+public class Fraction implements Comparable<Fraction> {
+
+	// Extension of Comparable from Java Library
+	@Override
+	public int compareTo(Fraction rhs) {
+		if (this.toDouble() > rhs.toDouble()) {
+			return 1;
+		} else if (this.toDouble() < rhs.toDouble()) {
+			return -1;
+		} else {
+			return 0;
+		}
 	}
-	
-	//Reduce based on Greatest Common Denominator
+
+	// Greatest Common Denominator
+	private long calculateGCD() {
+		long gcd = numerator;
+		long remainder = denominator;
+		while (remainder != 0) {
+			long temp = remainder;
+			remainder = gcd % remainder;
+			gcd = temp;
+		}
+		return gcd;
+	}
+
+	// Reduce based on Greatest Common Denominator
 	private void reduce() {
 		long gcd = calculateGCD();
 		numerator /= gcd;
 		denominator /= gcd;
 	}
-	
-	
-	
+
 	public long numerator;
 	public long denominator;
-	
-	
 
-	//Constructor
+	// Constructor
 	public Fraction() {
 		numerator = 0;
 		denominator = 1;
 	}
-	
-	//Constructor
+
+	// Constructor
 	public Fraction(long n, long d) throws Exception {
 		numerator = n;
 		denominator = d;
-		
+
 		reduce();
-		
+
 		if (denominator < 0) {
 			numerator *= -1;
 			denominator *= -1;
 		}
-		
+
 		if (denominator == 0) {
 			throw new Exception("Denominator is Zero");
 		}
-		
+
 	}
-	
-	
-	
+
 	public Fraction plus(Fraction rhs) throws Exception {
 		long newNumer = (numerator * rhs.denominator) + (rhs.numerator * denominator);
 		long newDenom = (denominator * rhs.denominator);
-		Fraction newFraction = new Fraction (newNumer, newDenom);
-		
+		Fraction newFraction = new Fraction(newNumer, newDenom);
+
 		return newFraction;
 	}
-	
+
 	public Fraction minus(Fraction rhs) throws Exception {
 		long newNumer = (numerator * rhs.denominator) - (rhs.numerator * denominator);
 		long newDenom = (denominator * rhs.denominator);
-		Fraction newFraction = new Fraction (newNumer, newDenom);
-		
+		Fraction newFraction = new Fraction(newNumer, newDenom);
+
 		return newFraction;
 	}
-	
+
 	public Fraction times(Fraction rhs) throws Exception {
 		long newNumer = (numerator * rhs.numerator);
 		long newDenom = (denominator * rhs.denominator);
-		Fraction newFraction = new Fraction (newNumer, newDenom);
-		
+		Fraction newFraction = new Fraction(newNumer, newDenom);
+
 		return newFraction;
 	}
-	
+
 	public Fraction dividedBy(Fraction rhs) throws Exception {
 		Fraction reciprocalFraction = rhs.reciprocal();
 		long newNumer = (numerator * reciprocalFraction.numerator);
 		long newDenom = (denominator * reciprocalFraction.denominator);
-		Fraction newFraction = new Fraction (newNumer, newDenom);
-		
+		Fraction newFraction = new Fraction(newNumer, newDenom);
+
 		return newFraction;
 	}
-	
-	
-	
+
 	public Fraction reciprocal() throws Exception {
 		long newNumer = denominator;
 		long newDenom = numerator;
 		Fraction newFraction = new Fraction(newNumer, newDenom);
-		
+
 		return newFraction;
 	}
-	
-	
-	
+
 	public String toString() {
 		String stringNumer = String.valueOf(numerator);
 		String stringDenom = Long.toString(denominator);
-		
+
 		return (stringNumer + "/" + stringDenom);
 	}
-	
+
 	public double toDouble() {
-		return (double) numerator/(double) denominator;
+		return (double) numerator / (double) denominator;
 	}
-	
-	
+
 }
--- 6011/hwjavafractions/src/junitTest.java
+++ 6011/hwjavafractions/src/junitTest.java
@@ -1,40 +1,58 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
 
 class junitTest {
 
 	@Test
 	public void test() throws Exception {
-		
-			Fraction test0 = new Fraction();
-			Fraction testHalf = new Fraction(1,0);
-			Fraction test4 = new Fraction(40, 10);
-			Fraction testNeg = new Fraction(9, -10);
-		
-			assertEquals("0/1", test0.toString());
-			assertEquals(4, test4.toDouble());
-			assertEquals("-9/10", testNeg.toString());
-			assertEquals("2/1", testHalf.reciprocal().toString());
-		
-			Fraction addedFracs = test0.plus(testHalf);
-			assertEquals(.5, addedFracs.toDouble());
-		
-			Fraction subtractedFracs = testNeg.minus(test4);
-			assertEquals("-49/10", subtractedFracs.toString());
-		
-			Fraction multipliedFracs = testHalf.times(testNeg);
-			assertEquals("-9/20", multipliedFracs.toString());
-		
-			Fraction dividedFracs = testHalf.dividedBy(testHalf);
-			assertEquals(1, dividedFracs.toDouble());
-			
-			try {
-				Fraction testException = new Fraction(0,0);
-				fail("Bad");
-			} catch (Exception e) {
-				System.out.println("Divide by Zero Error");
-			}
+
+		Fraction test0 = new Fraction();
+		Fraction testHalf = new Fraction(1, 2);
+		Fraction test4 = new Fraction(40, 10);
+		Fraction testNeg = new Fraction(9, -10);
+
+		assertEquals("0/1", test0.toString());
+		assertEquals(4, test4.toDouble());
+		assertEquals("-9/10", testNeg.toString());
+		assertEquals("2/1", testHalf.reciprocal().toString());
+
+		Fraction addedFracs = test0.plus(testHalf);
+		assertEquals(.5, addedFracs.toDouble());
+
+		Fraction subtractedFracs = testNeg.minus(test4);
+		assertEquals("-49/10", subtractedFracs.toString());
+
+		Fraction multipliedFracs = testHalf.times(testNeg);
+		assertEquals("-9/20", multipliedFracs.toString());
+
+		Fraction dividedFracs = testHalf.dividedBy(testHalf);
+		assertEquals(1, dividedFracs.toDouble());
+
+		try {
+			Fraction testException = new Fraction(0, 0);
+			fail("Bad");
+		} catch (Exception e) {
+			System.out.println("Divide by Zero Error");
+		}
+
+		assertEquals(test4.compareTo(testHalf), 1);
+		assertEquals(testHalf.compareTo(test4), -1);
+		assertEquals(testNeg.compareTo(testNeg), 0);
+
+		ArrayList<Fraction> fractionList = new ArrayList<Fraction>();
+		fractionList.add(test0);
+		fractionList.add(testHalf);
+		fractionList.add(test4);
+		fractionList.add(testNeg);
+
+		Collections.sort(fractionList);
+		assertEquals(fractionList.get(0), testNeg);
+		assertEquals(fractionList.get(3), test4);
+
 	}
 
 }
