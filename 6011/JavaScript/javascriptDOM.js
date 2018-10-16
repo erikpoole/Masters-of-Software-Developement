472e56af7b2b0006d58a5e68ed7a87497507fe53
Erik Poole <Mahkumazahn@gmail.com>
2018-10-15 23:18:24 -0600

    Most of HTML complete, NO CSS, NO YOUTUBE

--- 6011/JavaScript/javascriptDOM.js
+++ 6011/JavaScript/javascriptDOM.js
@@ -7,6 +7,7 @@ Use the DOM api to recreate the HTML/CSS page you wrote.
 
 'use strict';
 
+
 function addTextElement(elementType, text) {
     let element = document.createElement(elementType);
     let textNode = document.createTextNode(text);
@@ -17,14 +18,18 @@ function addTextElement(elementType, text) {
 addTextElement('h1', 'This is a Title!');
 addTextElement('p', 'This is a sentence!');
 addTextElement('b', 'This is a bold sentence!');
+document.body.appendChild(document.createElement('br'));
+document.body.appendChild(document.createElement('br'));
 
-// let element = document.createElement('p');
-// let text = document.createTextNode('The CRAP');
-// let element2 = document.createElement('abbr');
-// element2.setAttribute('title', 'Creative Renaissance Artists and Painters');
-// document.body.appendChild(element);
-// element.appendChild(text);
-// document.body.appendChild(element2);
+
+let abbreviation = document.createElement('abbr');
+let abbreviationText = document.createTextNode('The CRAP');
+abbreviation.setAttribute('Title', 'Creative Renaissance Artists and Painters')
+abbreviation.appendChild(abbreviationText);
+document.body.appendChild(abbreviation);
+document.body.appendChild(document.createElement('br'));
+document.body.appendChild(document.createElement('br'));
+
 
 let form = document.createElement('form');
 form.textContent = 'Are hotdogs Sandwiches?';
@@ -45,6 +50,7 @@ basicForm.appendChild(input2);
 basicForm.appendChild(text2);
 basicForm.appendChild(document.createElement('br'));
 
+
 let listMain = document.createElement('ol');
 
 function addListElement(text) {
@@ -60,35 +66,14 @@ addListElement('Profit!!!');
 document.body.appendChild(listMain);
 
 
+let link = document.createElement('a');
+let linktext = document.createTextNode('This is a link to the whole of human knowledge');
+link.href = 'https://www.wikipedia.org/';
+document.body.appendChild(link);
+link.appendChild(linktext);
+
 /*
 
-<body>
-
-    <h1> This is a Title! </h1>
-
-    <p> This is a sentence! </p>
-    <b> This is a bold sentence!</b>
-    <p>The <abbr title="Creative Renaissance Artists and Painters">CRAP</abbr></p>
-
-    <form>
-        Are hotdogs sandwiches? <br>
-        <input type="radio" name="hotdogquestion" value="Yes" checked> Yes <br>
-        <input type="radio" name="hotdogquestion" value="No"> Hell No <br>
-    </form>
-    <br>
-
-    <ol>
-        <li>Make a terrible looking webpage</li>
-        <li>???</li>
-        <li>Profit!!!</li>
-    </ol>
-
-
-    <br>
-    <a class=links href="https://www.wikipedia.org/"> 
-        This is a link to the whole of human knowledge </a>
-        <br>
-        <br>
 
         <iframe width="560" height="315" 
         src="https://www.youtube.com/embed/DLzxrzFCyOs" 
@@ -105,6 +90,5 @@ document.body.appendChild(listMain);
         frameborder="0" allow="autoplay; encrypted-media" 
         allowfullscreen></iframe>
 
-</body>
 
 */
