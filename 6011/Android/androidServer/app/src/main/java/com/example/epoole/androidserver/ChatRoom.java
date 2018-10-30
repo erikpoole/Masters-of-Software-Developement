2f5693a26a79e23f52ab369a305dace18bce965c
Erik Poole <Mahkumazahn@gmail.com>
2018-10-30 11:10:11 -0600

    updated websocket to work off of second Activity

--- 6011/Android/androidServer/app/src/main/java/com/example/epoole/androidserver/ChatRoom.java
+++ 6011/Android/androidServer/app/src/main/java/com/example/epoole/androidserver/ChatRoom.java
@@ -3,9 +3,19 @@ package com.example.epoole.androidserver;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketFactory;
+
+import java.io.IOException;
+
 public class ChatRoom extends AppCompatActivity {
+
+    public WebSocket ws = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,42 @@ public class ChatRoom extends AppCompatActivity {
         TextView roomTitle = findViewById(R.id.roomTitle);
         usernameTitle.setText("Username: " + username);
         roomTitle.setText("Room: " + room);
+
+        WebSocketFactory factory = new WebSocketFactory().setConnectionTimeout(5000);
+
+        try {
+            ws = factory.createSocket("ws://10.0.2.2:8080");
+            ws.addListener(new WebSocketAdapter() {
+
+                @Override
+                public void onTextMessage(WebSocket webSocket, String message) throws Exception {
+
+                    Log.d("TAG", "onTextMessage: " + message);
+                }
+            });
+
+            ws.connectAsynchronously();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (ws != null) {
+            ws.disconnect();
+            ws = null;
+        }
+    }
+
+
+    public void sendMessage(View v) {
+        if (ws.isOpen()) {
+            ws.sendText("Message from Android!");
+        }
     }
 }
--- 6011/Android/androidServer/app/src/main/java/com/example/epoole/androidserver/Login.java
+++ 6011/Android/androidServer/app/src/main/java/com/example/epoole/androidserver/Login.java
@@ -3,15 +3,10 @@ package com.example.epoole.androidserver;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import com.neovisionaries.ws.client.WebSocket;
-import com.neovisionaries.ws.client.WebSocketAdapter;
-import com.neovisionaries.ws.client.WebSocketFactory;
-
-import java.io.IOException;
 
 
 //More or less completely stolen from: en.proft.me/2018/05/10/quick-guide-websocket-communication-android
@@ -20,8 +15,6 @@ public class Login extends AppCompatActivity {
     public static final String USERNAME = "com.example.epoole.androidserver.USERNAME";
     public static final String ROOM = "com.example.epoole.androidserver.ROOM";
 
-
-    public WebSocket ws = null;
     public String room;
     public String username;
 
@@ -30,44 +23,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        WebSocketFactory factory = new WebSocketFactory().setConnectionTimeout(5000);
-
-        try {
-            ws = factory.createSocket("ws://10.0.2.2:8080");
-
-            ws.addListener(new WebSocketAdapter() {
-
-                @Override
-                public void onTextMessage(WebSocket webSocket, String message) throws Exception {
-
-                    Log.d("TAG", "onTextMessage: " + message);
-                }
-            });
-
-            ws.connectAsynchronously();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if (ws != null) {
-            ws.disconnect();
-            ws = null;
-        }
-    }
-
-    public void sendMessage(View v) {
-        if (ws.isOpen()) {
-            ws.sendText("Message from Android!");
-        }
-    }
 
     public void transferInput(View v) {
         Intent intent = new Intent(this, ChatRoom.class);
