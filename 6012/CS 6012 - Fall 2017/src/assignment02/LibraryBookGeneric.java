81c852156a19afc54ef378f76846e06a57030440
Erik Poole <Mahkumazahn@gmail.com>
2018-11-07 17:38:30 -0700

    Generic Library Class Introduced

---
+++ 6012/CS 6012 - Fall 2017/src/assignment02/LibraryBookGeneric.java
@@ -0,0 +1,36 @@
+package assignment02;
+
+import java.util.GregorianCalendar;
+
+public class LibraryBookGeneric<Type> extends Book {
+
+ private Type holder;
+ private GregorianCalendar dueDate;
+
+ public LibraryBookGeneric(long isbn, String author, String title) {
+  super(isbn, author, title);
+
+  holder = null;
+  dueDate = null;
+ }
+
+ public Type getHolder() {
+  return holder;
+ }
+
+ public GregorianCalendar getDueDate() {
+  return dueDate;
+ }
+
+ // currently dueDate = checkoutDate TODO
+ public void checkOut(Type newHolder, GregorianCalendar checkoutDate) {
+  holder = newHolder;
+  dueDate = checkoutDate;
+ }
+
+ public void checkIn() {
+  holder = null;
+  dueDate = null;
+ }
+
+}
--- 6012/CS 6012 - Fall 2017/src/assignment02/LibraryTest.java
+++ 6012/CS 6012 - Fall 2017/src/assignment02/LibraryTest.java
@@ -11,89 +11,113 @@ import java.util.Random;
  */
 public class LibraryTest {
 
-  public static void main(String[] args) {
-    // test an empty library
-    Library lib = new Library();
+ public static void main(String[] args) {
+  // test an empty library
+  Library lib = new Library();
 
-    if (lib.lookup(978037429279L) != null)
-      System.err.println("TEST FAILED -- empty library: lookup(isbn)");
-    ArrayList<LibraryBook> booksCheckedOut = lib.lookup("Jane Doe");
-    if (booksCheckedOut == null || booksCheckedOut.size() != 0)
-      System.err.println("TEST FAILED -- empty library: lookup(holder)");
-    if (lib.checkout(978037429279L, "Jane Doe", 1, 1, 2008))
-      System.err.println("TEST FAILED -- empty library: checkout");
-    if (lib.checkin(978037429279L))
-      System.err.println("TEST FAILED -- empty library: checkin(isbn)");
-    if (lib.checkin("Jane Doe"))
-      System.err.println("TEST FAILED -- empty library: checkin(holder)");
+  if (lib.lookup(978037429279L) != null)
+   System.err.println("TEST FAILED -- empty library: lookup(isbn)");
+  ArrayList<LibraryBook> booksCheckedOut = lib.lookup("Jane Doe");
+  if (booksCheckedOut == null || booksCheckedOut.size() != 0)
+   System.err.println("TEST FAILED -- empty library: lookup(holder)");
+  if (lib.checkout(978037429279L, "Jane Doe", 1, 1, 2008))
+   System.err.println("TEST FAILED -- empty library: checkout");
+  if (lib.checkin(978037429279L))
+   System.err.println("TEST FAILED -- empty library: checkin(isbn)");
+  if (lib.checkin("Jane Doe"))
+   System.err.println("TEST FAILED -- empty library: checkin(holder)");
 
-    // test a small library
-    lib.add(9780374292799L, "Thomas L. Friedman", "The World is Flat");
-    lib.add(9780330351690L, "Jon Krakauer", "Into the Wild");
-    lib.add(9780446580342L, "David Baldacci", "Simple Genius");
+  // test a small library
+  lib.add(9780374292799L, "Thomas L. Friedman", "The World is Flat");
+  lib.add(9780330351690L, "Jon Krakauer", "Into the Wild");
+  lib.add(9780446580342L, "David Baldacci", "Simple Genius");
 
-    if (lib.lookup(9780330351690L) != null)
-      System.err.println("TEST FAILED -- small library: lookup(isbn)");
-    if (!lib.checkout(9780330351690L, "Jane Doe", 1, 1, 2008))
-      System.err.println("TEST FAILED -- small library: checkout");
-    booksCheckedOut = lib.lookup("Jane Doe");
-    if (booksCheckedOut == null || booksCheckedOut.size() != 1
-        || !booksCheckedOut.get(0).equals(new Book(9780330351690L, "Jon Krakauer", "Into the Wild"))
-        || !booksCheckedOut.get(0).getHolder().equals("Jane Doe")
-        || !booksCheckedOut.get(0).getDueDate().equals(new GregorianCalendar(2008, 1, 1)))
-      System.err.println("TEST FAILED -- small library: lookup(holder)");
-    if (!lib.checkin(9780330351690L))
-      System.err.println("TEST FAILED -- small library: checkin(isbn)");
-    if (lib.checkin("Jane Doe"))
-      System.err.println("TEST FAILED -- small library: checkin(holder)");
+  if (lib.lookup(9780330351690L) != null)
+   System.err.println("TEST FAILED -- small library: lookup(isbn)");
+  if (!lib.checkout(9780330351690L, "Jane Doe", 1, 1, 2008))
+   System.err.println("TEST FAILED -- small library: checkout");
+  booksCheckedOut = lib.lookup("Jane Doe");
+  if (booksCheckedOut == null || booksCheckedOut.size() != 1
+    || !booksCheckedOut.get(0).equals(new Book(9780330351690L, "Jon Krakauer", "Into the Wild"))
+    || !booksCheckedOut.get(0).getHolder().equals("Jane Doe")
+    || !booksCheckedOut.get(0).getDueDate().equals(new GregorianCalendar(2008, 1, 1)))
+   System.err.println("TEST FAILED -- small library: lookup(holder)");
+  if (!lib.checkin(9780330351690L))
+   System.err.println("TEST FAILED -- small library: checkin(isbn)");
+  if (lib.checkin("Jane Doe"))
+   System.err.println("TEST FAILED -- small library: checkin(holder)");
 
-    // test a medium library
-    lib.addAll("Mushroom_Publishing.txt");
+  
+//********************************************************************************  
+//********************************************************************************  
+  
+  // test a medium library
+  lib.addAll("Mushroom_Publishing.txt");
 
-    // FILL IN
+  if (lib.lookup(100) != null) {
+   System.err.println("TEST FAILED -- medium library: lookupNotPresent(isbn)");
+  }
+  if (lib.lookup(9781843190875L) != null) {
+   System.err.println("TEST FAILED -- medium library: lookupPresent(isbn)");
+  }
+  
+  if (!(lib.lookup("eve").isEmpty())) {
+   System.err.println("TEST FAILED -- medium library: lookupNotPresent(name)");
+  }
+  
+  lib.checkout(9781843190875L, "eve", 1, 1, 1);
 
-    System.out.println("Testing done.");
+  if (lib.lookup("eve").isEmpty()) {
+   System.err.println("TEST FAILED -- medium library: lookupPresent(name)");
+  }
+  
+  lib.checkin("eve");
+  
+  if (!(lib.lookup("eve").isEmpty())) {
+   System.err.println("TEST FAILED -- medium library: lookupNotPresent(name)");
   }
 
-  /**
-   * Returns a library of "dummy" books (random ISBN and placeholders for author
-   * and title).
-   * 
-   * Useful for collecting running times for operations on libraries of varying
-   * size.
-   * 
-   * @param size
-   *          -- size of the library to be generated
-   */
-  public static ArrayList<LibraryBook> generateLibrary(int size) {
-    ArrayList<LibraryBook> result = new ArrayList<LibraryBook>();
+  
+  
+  
+  System.out.println("Testing done.");
+ }
 
-    for (int i = 0; i < size; i++) {
-      // generate random ISBN
-      Random randomNumGen = new Random();
-      String isbn = "";
-      for (int j = 0; j < 13; j++)
-        isbn += randomNumGen.nextInt(10);
+ /**
+  * Returns a library of "dummy" books (random ISBN and placeholders for author and title).
+  * 
+  * Useful for collecting running times for operations on libraries of varying size.
+  * 
+  * @param size -- size of the library to be generated
+  */
+ public static ArrayList<LibraryBook> generateLibrary(int size) {
+  ArrayList<LibraryBook> result = new ArrayList<LibraryBook>();
 
-      result.add(new LibraryBook(Long.parseLong(isbn), "An author", "A title"));
-    }
+  for (int i = 0; i < size; i++) {
+   // generate random ISBN
+   Random randomNumGen = new Random();
+   String isbn = "";
+   for (int j = 0; j < 13; j++)
+    isbn += randomNumGen.nextInt(10);
 
-    return result;
+   result.add(new LibraryBook(Long.parseLong(isbn), "An author", "A title"));
   }
 
-  /**
-   * Returns a randomly-generated ISBN (a long with 13 digits).
-   * 
-   * Useful for collecting running times for operations on libraries of varying
-   * size.
-   */
-  public static long generateIsbn() {
-    Random randomNumGen = new Random();
+  return result;
+ }
 
-    String isbn = "";
-    for (int j = 0; j < 13; j++)
-      isbn += randomNumGen.nextInt(10);
+ /**
+  * Returns a randomly-generated ISBN (a long with 13 digits).
+  * 
+  * Useful for collecting running times for operations on libraries of varying size.
+  */
+ public static long generateIsbn() {
+  Random randomNumGen = new Random();
 
-    return Long.parseLong(isbn);
-  }
+  String isbn = "";
+  for (int j = 0; j < 13; j++)
+   isbn += randomNumGen.nextInt(10);
+
+  return Long.parseLong(isbn);
+ }
 }
