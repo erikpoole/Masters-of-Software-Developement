b2978490381d230652288b844b3e32cccef2dd87
Erik Poole <Mahkumazahn@gmail.com>
2019-01-07 13:23:55 -0700

    Lots of problems, initial attempt

---
+++ 6013/CWarmup/CWarmup/functions.h
@@ -0,0 +1,20 @@
+//
+//  functions.h
+//  CWarmup
+//
+//  Created by Erik Poole on 1/7/19.
+//  Copyright © 2019 ErikPoole. All rights reserved.
+//
+
+#ifndef functions_h
+#define functions_h
+
+#include <stdio.h>
+
+#endif /* functions_h */
+
+int count_hex_digits(unsigned long arg);
+void swap(uint8_t arr[], int location1, int location2) ;
+
+unsigned long byte_sort (unsigned long arg);
+
---
+++ 6013/CWarmup/CWarmup/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CWarmup
+//
+//  Created by Erik Poole on 1/7/19.
+//  Copyright © 2019 ErikPoole. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "functions.h"
+
+int main(int argc, const char * argv[]) {
+    byte_sort(0xEE11EE);
+}
