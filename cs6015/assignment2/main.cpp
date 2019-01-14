11e097e6d8e1085d145189956e05a5eeb5434dc8
Erik Poole <Mahkumazahn@gmail.com>
2019-01-14 13:03:30 -0700

    Mostly done with documentaion, not very certain about code in GNU loop

--- cs6015/assignment2/main.cpp
+++ cs6015/assignment2/main.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 
+
 //checks to see if __GNUC__ (GNU Compiler) is defined, if so specifies information
 #ifdef __GNUC__
     //forward declaration of size_t WT for furture use
@@ -12,7 +13,7 @@
 #endif
 
 //copies n bytes from src to dest - unlike memcpy allows for overlap of src and dest
-//takes generic pointer to destination, constant generic pointer to source, and a size in bytes
+//takes generic pointer to destination, constant generic pointer to source, and a size based on an unsigned int (not consistent between OSes)
 //returns a generic pointer to the location the destination pointer points to
 void *memmove(void *dest, const void *src, size_t n)
 {
@@ -24,31 +25,51 @@ void *memmove(void *dest, const void *src, size_t n)
     //if the pointers point to the same location, no need to copy - return destination pointer
     if (d==s) return d;
     
+    //if the pointer to the source minus the pointer to the destination minus the length is less than the length times -2
+    //then there isn't overlap and memcpy can be called instead
+    //not sure why the qualification is s-d-n <= -2*n instead of s-d <= -n ???
     if ((uintptr_t)s-(uintptr_t)d-n <= -2*n) return memcpy(d, s, n);
     
+    //if destination is before source in memory
     if (d<s) {
-#ifdef __GNUC__
-        if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
-            while ((uintptr_t)d % WS) {
-                if (!n--) return dest;
-                *d++ = *s++;
+        //checks for presence of GNU compiler and handles optimally for that case
+        #ifdef __GNUC__
+            //if the pointers to s and d are aligned in relation to the size of one unsized int (again, varies based on OS)
+            if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
+                //evaluates to true if non-zero
+                
+                
+                
+                while ((uintptr_t)d % WS) {
+                    //in each loop, n is decremented and if n == 0, returns destination pointer since copy is complete
+                    if (!n--) return dest;
+                    //in each loop, pointer value at destination is set equal to the pointer value from the source
+                    //and both pointers are incremented
+                    *d++ = *s++;
+                }
+                //until n > sizeof(WT) n is decrimented by one unsigned int and both destination and source pointers are incremented
+                for (; n>=WS; n-=WS, d+=WS, s+=WS)
+                    //casts and derefernces pointers and then copies value at source to destination
+                    *(WT *)d = *(WT *)s;
             }
-            for (; n>=WS; n-=WS, d+=WS, s+=WS) *(WT *)d = *(WT *)s;
-        }
-#endif
+            #endif
+        //if not using the GNU conpiler - dererences both pointers and copies value at source to destination, then increments both
         for (; n; n--) *d++ = *s++;
     } else {
-#ifdef __GNUC__
-        if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
-            while ((uintptr_t)(d+n) % WS) {
-                if (!n--) return dest;
-                d[n] = s[n];
+        #ifdef __GNUC__
+            if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
+                while ((uintptr_t)(d+n) % WS) {
+                    if (!n--) return dest;
+                    d[n] = s[n];
+                }
+                while (n>=WS) n-=WS, *(WT *)(d+n) = *(WT *)(s+n);
             }
-            while (n>=WS) n-=WS, *(WT *)(d+n) = *(WT *)(s+n);
-        }
-#endif
+        #endif
         while (n) n--, d[n] = s[n];
     }
     
     return dest;
 }
+
+
+
