83a70e0d25c4cd6a7097052fdc24a8c0a7d27788
Erik Poole <Mahkumazahn@gmail.com>
2019-03-22 09:26:00 -0600

    Deleted code

--- 6013/threadsWarmup/kernelMain.cpp
+++ 6013/threadsWarmup/kernelMain.cpp
@@ -6,40 +6,6 @@
 //  Copyright © 2019 ErikPoole. All rights reserved.
 //
 
-/*
- Write equivalent programs using C++ + std::thread, and C + user level threads that:
- 
- take as commandline parameters the number of thread pairs, and the number of iterations per thread
- start up the specified number of thread pairs (one starter + one finisher in each pair)
- join all the threads
- Benchmark the two implementations. Which is faster? Use a profiler (Instrument's time profiler is good) to explore what's happening when each program runs. Include a short description of your findings.
- 
- 
- void starter( start ,  iters, int* shared):
- assert(start is even)
- *shared = start;
- for(i = 0 .. iters){
- while( *shared is odd){ yield(); }
- assert(shared == start + 2*i)
- ++(*shared)
- }
- 
- void finisher( start, iters, int* shared):
- 
- for(int i = 0 .. iters){
- while( *shared is even) { yield(); }
- assert( shared == start + 2*i + 1)
- ++(*shared)
- }
- 
- 
- 
- C portion - void* f(void*)
- pass struct* to thread constructor
- 
- writeup - with different number of threads through profiler and look at where hot spots are
- 
- */
 
 #include <atomic>
 #include <iostream>
